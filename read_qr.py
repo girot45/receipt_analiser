0caaa9b96917ef2889cb4929abe8792e44c5571f
girot45 <98813117+girot45@users.noreply.github.com>
2023-08-14 01:48:02 +0300

    Update read_qr.py

--- read_qr.py
+++ read_qr.py
@@ -1,5 +1,4 @@
 import cv2
-import requests
 
 from pyzbar.pyzbar import decode
 
@@ -9,16 +8,12 @@ def decode_qr_code(image_path):
     gray_image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2GRAY)
 
     # Поиск QR-кодов на изображении
-    # decoded_object = decode(gray_image)
+    decoded_object = decode(gray_image)
 
-    url = f"https://dewiar.com/scanner/?code=image&url=data:{gray_image}"
-    r = requests.post(url)
-    print(r.content)
-    return None
-    # if len(decoded_object) != 1:
-    #     return None
-    # else:
-    #     return decoded_object[0].data.decode('utf-8')
+    if len(decoded_object) != 1:
+        return None
+    else:
+        return decoded_object[0].data.decode('utf-8')
 
 
 
